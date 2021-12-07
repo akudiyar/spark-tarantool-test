7b3d1140abc17f51272162e0da4f529893598ea5
Tatiana Varnavskaya <varnavskaya@gnivc.ru>
2021-12-07 11:20:39 +0300

    Initial commit

---
+++ src/main/scala/TestTarantool.scala
@@ -0,0 +1,79 @@
+package ru.eiap.tarantool
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import io.tarantool.driver.api.conditions.Conditions
+import io.tarantool.driver.api.tuple.{DefaultTarantoolTupleFactory, TarantoolTuple}
+import io.tarantool.driver.mappers.DefaultMessagePackMapperFactory
+import io.tarantool.spark.connector.toSparkContextFunctions
+
+
+
+import scala.collection.JavaConverters.seqAsJavaListConverter
+
+
+object TestTarantool extends App {
+
+  // 1. Set up the Spark session
+  val spark = SparkSession.builder()
+    .appName("test tarantool")
+    .master("local[1]")
+    .config("tarantool.hosts", "127.0.0.1:3301")
+    .config("tarantool.username", "admin")
+    .config("tarantool.password", "password")
+    .enableHiveSupport()
+    .getOrCreate()
+
+
+  val sc = spark.sparkContext
+
+  // 2. Load the whole space
+  val rdd: Array[TarantoolTuple] = sc.tarantoolSpace("test_space", Conditions.any()).collect()
+
+  // 3. Filter using conditions
+  // This mapper will be used implicitly for tuple conversion
+  val mapper = DefaultMessagePackMapperFactory.getInstance().defaultComplexTypesMapper()
+
+  val startTuple = new DefaultTarantoolTupleFactory(mapper).create(List(1).asJava)
+  val cond: Conditions = Conditions
+    .indexGreaterThan("index_id", List(1).asJava)
+    .withLimit(2)
+    .startAfter(startTuple)
+  val tuples: Array[TarantoolTuple] = sc.tarantoolSpace("test_space", cond).collect()
+
+  // 4. Load the whole space into a DataFrame
+  val df = spark.read
+    .format("org.apache.spark.sql.tarantool")
+    .option("tarantool.space", "test_space")
+    .load()
+
+  // Space schema from Tarantool will be used for mapping the tuple fields
+  val tupleIDs: Array[Any] = df.select("id").rdd.map(row => row.get(0)).collect()
+
+  // 5. Write a Dataset to a Tarantool space
+
+  val ds = spark.sql(
+    """
+      |select 1 as id, 1 as bucketId, 'Don Quixote' as bookName, 'Miguel de Cervantes' as author, 1605 as year union all
+      |select 2, 1, 'The Great Gatsby', 'F. Scott Fitzgerald', 1925 union all
+      |select 2, 1, 'War and Peace', 'Leo Tolstoy', 1869
+      |""".stripMargin)
+
+
+  // Write to the space. Different modes are supported
+  ds.write
+    .format("org.apache.spark.sql.tarantool")
+    .mode(SaveMode.Overwrite)
+    .option("tarantool.space", "test_space")
+    .save()
+
+  sc.stop()
+  spark.close()
+}
+
+case class Book(
+                 id: Int,
+                 bucketId: Int,
+                 bookName: String,
+                 author: String,
+                 year: Int
+               )
